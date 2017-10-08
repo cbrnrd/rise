bc379a62f23c832bd6f14ddb64bc1b9e88e72556
thecarterb <cbawsome77@gmail.com>
2017-10-08 00:04:03 -0400

    Add https support to the client

--- lib/core/transport.rb
+++ lib/core/transport.rb
@@ -4,8 +4,7 @@
 require 'rex/text'
 require 'uri'
 require 'json'
-require 'http'
-
+require 'net/http'
 module Rise
   module Transport
 
@@ -24,22 +23,23 @@ module Rise
       end
 
       def upload!(*)
-        upload_uri_base = "http://rise.sh:8080/api/v1/#{@uuid}"
-        access_uri = "http://rise.sh/#{@uuid}"
+        upload_uri_base = "https://rise.sh:8080/api/v1/#{@uuid}"
+        access_uri = "https://rise.sh/#{@uuid}"
         uri = ''
 
         # This sorts the files by (file path) length.
         # It is supposed to make the server make the first layer of files
         # before the rest of the layers.
         ordered_files = files.sort_by(&:length)
+        
         ordered_files.each do |f|
           isdir = File.directory?(f)
           final_path = File.absolute_path(f).gsub(File.expand_path(folder_path), '')
-          uri = URI.parse("#{upload_uri_base}/#{final_path}?dir=#{isdir}")
+          http = Net::HTTP.new(upload_uri_base, 8080)
           begin
-            HTTP.put(uri.to_s, :body => File.read(f))
+            http.send_request('PUT', "#{final_path}?dir=#{isdir}", File.read(f))
           rescue Errno::EISDIR
-            HTTP.put(uri.to_s, :body => '')
+            http.send_request('PUT', "#{final_path}?dir=#{isdir}", '')
             next
           end
         end
