d87aaf90a59c2f47b5081a7a02337556cb25f204
thecarterb <cbawsome77@gmail.com>
2017-10-08 00:52:17 -0400

    Hopefully fix all immediate bugs

--- server/redirect_service.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-
-set :port, 80
-set :environment, :production
-
-get '*' do
-  redirect('https://rise.sh')
-end
-
--- server/upload_server.rb
+++ server/upload_server.rb
@@ -8,49 +8,39 @@ require 'thin'
 # Other useful stuff
 require 'fileutils'
 
-#class UploadServer < Sinatra::Base
-  # Set sinatra settings here
-  set :environment, :production
-  set :server, 'thin'
-  set :port, 8080
-  set :show_exceptions, true if development?
+# Set sinatra settings here
+set :ssl_certificate, '/etc/letsencrypt/archive/rise.sh-0001/fullchain1.pem'
+set :ssl_key, '/etc/letsencrypt/archive/rise.sh-0001/privkey1.pem'
+set :environment, :production
+set :server, 'thin'
+set :port, 8080
+set :show_exceptions, true if development?
 
-  FileUtils.mkdir('/var/www/rise.sh') if !File.directory?('/var/www/rise.sh')
+PUBLIC_FOLDER = File.join(Dir.home, 'rise-server-public').freeze
 
-  put '/api/v1/:uuid/*' do |uuid, path|
-    isdir = params[:dir]
-    if File.directory?(File.join('/var', 'www', 'rise.sh', uuid))
-      if isdir == "true"
-        FileUtils.mkdir(File.join('/var', 'www', 'rise.sh', uuid, path))
-        return
-      end
-      File.open(File.join('/var', 'www', 'rise.sh', uuid, path), 'w+') do |f|
-        request_body = request.body.read
-        f.puts(request_body)
-      end
-    else
-      FileUtils.mkdir(File.join('/var', 'www', 'rise.sh', uuid))
-      puts Paint["[#{Time.now}] Creating initial folder with uuid: #{uuid}", :blue]
-      if isdir == "true"
-        FileUtils.mkdir(File.join('/var', 'www', 'rise.sh', uuid, path))
-        return
-      end
-      File.open(File.join('/var', 'www', 'rise.sh', uuid, path), 'w+') do |f|
-        request_body = request.body.read
-        f.puts(request_body)
-      end
+FileUtils.mkdir(PUBLIC_FOLDER) if !File.directory?(PUBLIC_FOLDER)
+
+put '/api/v1/:uuid/*' do |uuid, path|
+  isdir = params[:dir]
+  if File.directory?(File.join(PUBLIC_FOLDER, uuid))
+    if isdir == "true"
+      FileUtils.mkdir(File.join(PUBLIC_FOLDER, uuid, path))
+      return
+    end
+    File.open(File.join(PUBLIC_FOLDER, uuid, path), 'w+') do |f|
+      request_body = request.body.read
+      f.puts(request_body)
+    end
+  else
+    FileUtils.mkdir(File.join(PUBLIC_FOLDER, uuid))
+    puts Paint["[#{Time.now}] Creating initial folder with uuid: #{uuid}", :blue]
+    if isdir == "true"
+      FileUtils.mkdir(File.join(PUBLIC_FOLDER, uuid, path))
+      return
+    end
+    File.open(File.join(PUBLIC_FOLDER, uuid, path), 'w+') do |f|
+      request_body = request.body.read
+      f.puts(request_body)
     end
   end
-
-#  def self.run!
- #   super #do |server|
-      #server.ssl = true
-      #server.ssl_options = {
-      #  :cert_chain_file  => '/etc/letsencrypt/archive/rise.sh-0001/fullchain1.pem',
-      #  :private_key_file => '/etc/letsencrypt/archive/rise.sh-0001/privkey1.pem',
-      #  :verify_peer      => false
-      #}
-    #end
-  #end
-  #run! if app_file == $0
-#end
+end
