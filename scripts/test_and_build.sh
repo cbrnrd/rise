c3275719d679f5bac51f70a068ca9d31696ebb89
cbrnrd <cbawsome77@gmail.com>
2017-11-06 17:51:16 -0500

    Add test and build utility script

---
+++ scripts/test_and_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ..
+rspec
+if [[ "$?" -ne "0"  ]]; then
+  echo -e "\033[0;31mTests failed, not building the gem\033[0;0m"
+  exit $?
+fi
+
+rubocop #--fail-level W
+if [[ "$?" -ne "0"  ]]; then
+  echo -e "\033[0;31mRubocop failed, not building the gem\033[0;0m"
+  exit $?
+fi
+
+rake build
