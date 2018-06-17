02701144e3eca41f800a63392d3e29e515dbbbcc
cbrnrd <cbawsome77@gmail.com>
2018-06-17 04:38:19 +0000

    Revert "Server updates"
    This reverts commit 766e480f76dd053b07a980f4809414010a239c32.

--- server/s.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require './sinatra-ssl'
-
-set :port, 443
-set :environment, :production
-
-get '/' do
-  'hello world'
-end
--- server/server.rb
+++ server/server.rb
@@ -8,49 +8,25 @@ webrick_options = {
   DocumentRoot:    '/root/rise-server-public',
   SSLEnable:       true,
   SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE,
-  SSLCertificate:  OpenSSL::X509::Certificate.new(File.read('/etc/letsencrypt/archive/rise.sh/cert3.pem')),
-  SSLPrivateKey:   OpenSSL::PKey::RSA.new(File.read('/etc/letsencrypt/archive/rise.sh/privkey3.pem')),
+  SSLCertificate:  OpenSSL::X509::Certificate.new(File.read('/etc/letsencrypt/archive/rise.sh/cert1.pem')),
+  SSLPrivateKey:   OpenSSL::PKey::RSA.new(File.read('/etc/letsencrypt/archive/rise.sh/privkey1.pem')),
   SSLCertName:     [['US', WEBrick::Utils.getservername]]
 }
 
 fork do
   require 'sinatra'
-  require 'paint'
-  require './create_release'
 
   set :port, 80
   set :environment, :production
   set :logging, false
 
-  #$stderr.puts "\n[Redirect service] Starting redirect microservice with pid: #{Process.pid}"
+  puts "\n[Redirect service] Starting redirect microservice with pid: #{Process.pid}"
 
-  get '/api' do
-    # TODO
-    # After making the full API docs, this will redirect to the https version
-    # of that page.
-    'You got to the Rise API!'
+  get '*' do |path|
+    redirect("https://rise.sh#{path}")
   end
-
-  put '/api/v1/:uuid/*' do |uuid, path|
-    $stderr.puts Paint["API request to #{request.path_info} from #{Paint[request.ip, :blue]}", :red]
-    begin
-      if request.env['HTTP_AUTHORIZATION'].nil?
-        return JSON.pretty_generate({'code' => 401, 'message' => 'Unauthorized'})
-      end
-      CreateRelease.run(directory: params[:dir], uuid: uuid, path: path, req: request, key: request.env['HTTP_AUTHORIZATION'])
-    rescue StandardError => e
-      return JSON.pretty_generate({'code' => 500, 'message' => e.message})
-    end
-  end
-
-  # Catch everything else
-#  get '*' do |path|
-#    $stderr.puts Paint["Redirecting http -> https (#{request.ip})", :red]
-#    redirect("https://rise.sh#{path}")
-#  end
 end
 
-# Show a cool error page instead of a boring one
 module WEBrick
   class HTTPResponse
     def create_error_page
@@ -60,13 +36,4 @@ module WEBrick
   end
 end
 
-server = WEBrick::HTTPServer.new(webrick_options)
-
-# Add login path
-server.mount_proc '/login' do |req, res|
-  res.body = File.read('/root/rise-server-public/templates/login.html')
-end
-
-server.start
-
-
+WEBrick::HTTPServer.new(webrick_options).start
