e98e13b0d7e478d388ebcf4219af707c71c05533
cbrnrd <cbawsome77@gmail.com>
2019-02-21 11:36:20 -0500

    Add some verbose output

--- lib/core/text.rb
+++ lib/core/text.rb
@@ -18,7 +18,7 @@ module Rise
     # Prints +msg+ if the +RISE_VERBOSE+ environment variable is set to 'yes' (set with --verbose)
     #
     def self.vputs(msg='')
-      puts msg if ENV['RISE_VERBOSE'] == 'yes'
+      puts("[" + Paint["VERBOSE"] + "] - " + msg) if ENV['RISE_VERBOSE'] == 'yes'
     end
   end
 end
